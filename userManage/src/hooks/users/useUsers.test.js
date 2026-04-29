918165d67c7650a23fe7be6b2cadf90c5f8b3546
EL-ADRIRHI Driss <Driss.EL-ADRIRHI@akkodis.com>
2026-04-29 12:02:40 +0100

    test call redux action

--- userManage/src/hooks/users/useUsers.test.js
+++ userManage/src/hooks/users/useUsers.test.js
@@ -1,27 +1,52 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
+import { useSelector } from "react-redux";
 
 import useUsers from "./useUsers";
 import { customRenderHook } from "../../utils";
-import { useSelector } from "react-redux";
+import {
+  fetchUsersError,
+  fetchUsersRequest,
+  fetchUsersSuccess,
+} from "../../business/usersReducer/actions";
 
 jest.mock("react-redux", () => ({
   ...jest.requireActual("react-redux"),
   useSelector: jest.fn(),
 }));
 
+jest.mock("../../business/usersReducer/actions", () => ({
+  ...jest.requireActual("../../business/usersReducer/actions"),
+  fetchUsersRequest: jest.fn(), // () => {}
+  fetchUsersSuccess: jest.fn(),
+  fetchUsersError: jest.fn(),
+}));
+
 describe("useUsers", () => {
   beforeEach(() => {
     useSelector.mockReturnValue({ data: [], loading: true, error: null });
     fetch.mockResolvedValue({ ok: 200, json: () => [] });
+    fetchUsersRequest.mockReturnValue({ type: "foo_type" });
+    fetchUsersSuccess.mockReturnValue({ type: "foo_type2" });
+    fetchUsersError.mockReturnValue({ type: "foo_type2" });
   });
 
-  it("render correctly", () => {
+  it("fetch users when it's empty", async () => {
+    const expectedUsers = [{ id: 1 }];
+    fetch.mockResolvedValue({ ok: 200, json: () => expectedUsers });
     const { result } = customRenderHook(useUsers);
 
     expect(result.current.loading).toBe(true);
     expect(result.current.users).toEqual([]);
     expect(result.current.error).toBe(null);
     expect(fetch).toHaveBeenNthCalledWith(1, "http://localhost:8181/api/users");
+
+    expect(fetchUsersRequest).toHaveBeenCalledTimes(1);
+
+    await waitFor(() => {
+      expect(fetchUsersSuccess).toHaveBeenCalledTimes(1);
+    });
+
+    // expect(fetchUsersSuccess).toHaveBeenNthCalledWith(1, expectedUsers);
     // toEqual => deep equality
     // toBe => ref, str, number
   });
